da7a6e420774a2c45cc1de04b0a07476b00c33ff
googs1025 <googs1025@gmail.com>
2022-10-30 12:11:28 +0800

    fix: 增加json解析str方法

--- src/pods.go
+++ src/pods.go
@@ -1,11 +1,13 @@
 package src
 
 import (
+	"encoding/json"
 	"fmt"
 	"k8s.io/api/admission/v1"
 	coreV1 "k8s.io/api/core/v1"
 	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/klog/v2"
+	"log"
 )
 
 func AdmitPods(ar v1.AdmissionReview) *v1.AdmissionResponse {
@@ -35,10 +37,12 @@ func AdmitPods(ar v1.AdmissionReview) *v1.AdmissionResponse {
 
 	reviewResponse := v1.AdmissionResponse{}
 
+	// 如果准入时，发现pod名，直接拒绝
 	if pod.Name=="shenyi"{
 		reviewResponse.Allowed = false
 		reviewResponse.Result = &metaV1.Status{Code:503,Message: "pod name cannot be shenyi"}
 	}else{
+		// 其他就准入，并且采用patch操作
 		reviewResponse.Allowed = true
 		reviewResponse.Patch = patchContainerImage()
 		pt := v1.PatchTypeJSONPatch
@@ -71,3 +75,64 @@ func patchContainerImage() []byte {
 ]`
 	return []byte(str)
 }
+
+type PatchOperate struct {
+	Op string `json:"op"`
+	Path string `json:"path"`
+	Value string `json:"value"`
+}
+
+type InjectionOperate struct {
+	Op string `json:"op"`
+	Path string `json:"path"`
+	Value []*Value `json:"value"`
+}
+
+type Value struct {
+	Name string `json:"name"`
+	Image string `json:"image"`
+	Command []string `json:"command"`
+}
+
+func patchContainerImage2() []byte {
+	patch := &PatchOperate{
+		Op: "replace",
+		Path: "/spec/containers/0/image",
+		Value: "nginx:1.19-alpine",
+	}
+	b1, err := json.Marshal(patch)
+	if err != nil {
+		log.Println(err)
+		return []byte{}
+	}
+
+	valueList := make([]*Value, 0)
+	value := &Value{
+		Name: "myInjectionImage",
+		Image: "busybox:1.28",
+		Command: []string{"sh", "-c", "echo The app is running!"},
+	}
+	valueList = append(valueList, value)
+
+	injection := &InjectionOperate{
+		Op: "add",
+		Path: "/spec/initContainers",
+		Value: valueList,
+	}
+	b2, err := json.Marshal(injection)
+	if err != nil {
+		log.Println(err)
+		return []byte{}
+	}
+
+	resString := "[" + string(b1) + "," + string(b2) + "]"
+
+	fmt.Println(resString)
+	res := []byte(resString)
+
+	return res
+
+
+
+}
+
---
+++ src/pods_test.go
@@ -0,0 +1,9 @@
+package src
+
+import (
+	"testing"
+)
+
+func TestStruct2Json(t *testing.T) {
+	_ = patchContainerImage2()
+}
